69230067d753f451b05bd4b8cf20588992b821d0
keita <keita@MacBook-Pro-de-keita.local>
2021-09-17 09:32:05 +0200

    get et addtrans

--- app/src/main/java/ama/Dictionary.java
+++ app/src/main/java/ama/Dictionary.java
@@ -1,7 +1,14 @@
 
 package ama;
+
+import java.util.HashMap;
+import java.util.List;
+
 public class Dictionary {
+
 	public String name;
+	private HashMap<String, List<String>> translations = new HashMap<>();
+
 
 	public Dictionary(String name){
 		this.name = name;
@@ -13,13 +20,24 @@ public class Dictionary {
 		return name;
 
 	}
-	public  boolean isEmpty(){
-		if( this.getName()== null){
-			return  true ;
-		}else{
-			return false;
+	public List<String> getTranslation(String elem){
+		return this.translations.get(elem);
+	}
+	public List<String> addTranslation(String firstElem, List<String>ele_lis){
+		return this.translations.put(firstElem,ele_lis);
+	}
+
+
+
+	public boolean isEmpty(){
+		if(this.translations.size() == 0){
+			return true;
 		}
 
+		return false;
+	}
+
+	public void addTranslationNew(String ele1, String ele2) {
 	}
 }
 
--- app/src/test/java/ama/DictionaryTest.java
+++ app/src/test/java/ama/DictionaryTest.java
@@ -1,20 +1,46 @@
 package ama;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public  class DictionaryTest {
     Dictionary dic = new Dictionary("Example");
+    @Before
+    public void initialize() {
+        List<String> tradui = new ArrayList<>();
+        tradui.add("S1");
+        tradui.add("S2");
+        tradui.add("s3");
+        dic.addTranslation("chien",tradui);
+
+        List<String> tradui_c = new ArrayList<>();
+        tradui_c.add("chien");
+        tradui_c.add("chien2");
+        tradui_c.add("chien3");
+        dic.addTranslation("chien",tradui_c);
+    }
+
+
+
+
+    @Test
     public void testDictionaryName() {
-        assertThat(dic.getName() ,equalTo("Example"));
-    }
-    public void testDictionaryIsEmpy() {
-        assertThat(dic.isEmpty() ,equalTo("Example"));
+        assertThat(dic.getName(), equalTo("Example"));
     }
 
+    @Test
+    public void testDictionaryIsEmpty() {
+        assertThat(dic.isEmpty(), equalTo("false"));
 
 
-
+    }
 }
 
 
